5e88bc02728f76c5e94f786050f916ea6b1badcc
shifttag <ehdgus0837@naver.com>
2024-01-05 11:29:00 +0900

    20240105 java

--- JAVA_BT1228/src/Git.java
+++ JAVA_BT1228/src/Git.java
@@ -15,11 +15,11 @@ public class Git {
 		// - local repository
 		// : 개인 PC에 저장되는 저장소 (push)
 		
-		// 2. GitHub
-		// : Git을 사용하는 프로젝트를 지원하는 웹 호스팅 서비스
-		
 		// - remote repository
 		// : 다른 개발자와 공유하는 저장소 (pull)
+		
+		// 2. GitHub
+		// : Git을 사용하는 프로젝트를 지원하는 웹 호스팅 서비스
 		
 		// - git push와 git pull 명령어를 사용하여 로컬 리포지토리와 원격 리포지토리 간의 동기화
 		
@@ -45,6 +45,55 @@ public class Git {
 		// - 등록 확인
 		// git config --global user.name
 		// git config --global user.email
+		
+		
+				
+		
+		// 원격 리포지토리 (GitHub) 연결
+		// : 로컬 Git 리포지토리에서 원격 리포지토리를 연결
+		// : GitHub 리포지토리 페이지에서 제공하는 URL 사용
+		
+		// 리포지토리 초기화 및 기본 운영
+		// a. 리포지토리 초기화 및 기본 운영
+		// : 새로운 Git 리포지토리 생성
+		// : git init 명령어 사용
+		// : 새로운 Git 리포지토리를 초기화 하고 .git 디렉토리를 생성
+		
+		// b. 상태확인 (status)
+		// : 리포지토리의 현재 상태 확인
+		// : 업로드(push) 하지 않은 파일이 있는지 확인
+		// : git status 명렁어 사용
+		 
+		// c. 파일 추가 (add)
+		// : git add .
+		// : 마침표는 전체 변경 사항을 모두 업로드
+		
+		// d. 커밋(commit)
+		// : 변경 사항을 리포지토리에 기록하기 위한 명령어
+		// git commit -m "커밋메시지 - 코드에대한 설명"
+		// git commit -m "20240104 jajva"
+		
+		// e. 코드 푸시 (push)
+		// : 로컬 리포지토리에서 변경사항을 원격 리포지토리에 저장
+		// git push origin main
+		
+		//=============================================//
+		
+		// 원격 리포지토리(깃허브) 연결
+		  
+		// >> 연동 전 해당 리포지토리의 파일을 받아올 폴더에
+		// : 리포지토리 초기화(init) 실시 : git init
+		 
+		// >> branch 변경(main)
+		// : git branch -M main
+		  
+		// >> 로컬 Git 리포지토리에서 원격 리포지토리를 연결
+		// : GitHub에서 제공하는 URL 사용
+		// : git remote add origin 원격리포지토리URL
+		
+		// >> 원격 리포지토리에서 파일 당겨오기
+		// : git pull origin main
+		
 		
 	}
 
